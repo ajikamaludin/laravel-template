fc59b1e80a7583d9223eaf2f646030f4f6e0ba50
ajikamaludin <aji19kamaludin@gmail.com>
2023-11-22 21:54:44 +0700

    fix bug sonarqube

--- routes/web.php
+++ routes/web.php
@@ -46,4 +46,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require_once __DIR__ . '/auth.php';
