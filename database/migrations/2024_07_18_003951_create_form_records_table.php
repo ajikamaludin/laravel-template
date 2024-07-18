9f5875073ad788acc3b50c5979e014d553f758d8
Aji Kamaludin <aji19kamaludin@gmail.com>
2024-07-18 11:45:45 +0700

    bitfix

--- database/migrations/2024_07_18_003951_create_form_records_table.php
+++ database/migrations/2024_07_18_003951_create_form_records_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('form_data');
+        Schema::dropIfExists('form_records');
     }
 };
