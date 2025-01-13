275bad1f0c045ccb909d80fac8a62f96107074ec
Aji Kamaludin <aji19kamaludin@gmail.com>
2025-01-13 16:19:54 +0700

    tidy up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Vite::prefetch(concurrency: 3);
+
         if (env('LOG_SQL_ENABLE', false)) {
             DB::listen(function ($query) {
                 Log::info(
